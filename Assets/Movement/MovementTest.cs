5c6f14d310fecf176530665736140dbfc411e544
huangwwmm <hwm@huangwm.com>
2019-02-27 18:25:55 +0800

    使用Graphics.DrawMeshInstancedIndirect

--- Assets/Movement/MovementTest.cs
+++ Assets/Movement/MovementTest.cs
@@ -7,20 +7,27 @@ public class MovementTest : MonoBehaviour
     private int INT_TRUE = 1;
     private int INT_FALSE = 0;
     private static readonly int SHADER_PROPERTYID_DELTATIME = Shader.PropertyToID("_DeltaTime");
+    private static Bounds UNLIMITED_BOUNDS = new Bounds(Vector3.zero, new Vector3(999999f, 999999f, 999999f));
 
-    public GameObject RolePrefab;
+    public Camera MainCamera;
+    public Mesh RoleMesh;
+    public Material RoleMaterial;
     public int RoleCount;
     public Vector2 GroundHalfSize;
     public ComputeShader MovementComputeShader;
     public float MoveSpeed;
 
-    private RoleTest[] m_Roles;
+    private RoleState[] m_RoleStates;
+    private ComputeBuffer m_CB_RoleState;
 
-    private CB_I_RoleCommand[] m_RoleCommands;
-    private ComputeBuffer m_CB_I_RoleCommand;
+    private RoleCommand[] m_RoleCommands;
+    private ComputeBuffer m_CB_RoleCommand;
 
-    private CB_IO_RoleState[] m_RoleStates;
-    private ComputeBuffer m_CB_IO_RoleState;
+    private RoleResult[] m_RoleResults;
+    private ComputeBuffer m_CB_RoleResult;
+
+    private uint[] m_RoleArgs;
+    private ComputeBuffer m_CB_RoleArgs;
 
     /// <summary>
     /// UNDONE 当前帧发送的Request，下一帧未必完成。所以这里要改成数组，一次发多个，以保证每帧都能拿到数据
@@ -30,49 +37,67 @@ public class MovementTest : MonoBehaviour
 
     protected void Awake()
     {
-        m_Roles = new RoleTest[RoleCount];
-        m_RoleStates = new CB_IO_RoleState[RoleCount];
-        m_RoleCommands = new CB_I_RoleCommand[RoleCount];
+        m_CS_UpdateKernel = MovementComputeShader.FindKernel("Update");
+
+        m_RoleStates = new RoleState[RoleCount];
+        m_RoleCommands = new RoleCommand[RoleCount];
+        m_RoleResults = new RoleResult[RoleCount];
         for (int iRole = 0; iRole < RoleCount; iRole++)
         {
-            Vector3 position = RandPositionOnGround();
-            GameObject iterRole = Instantiate(RolePrefab, position, Quaternion.identity, null);
-            m_Roles[iRole] = iterRole.transform.GetComponent<RoleTest>();
-            m_RoleStates[iRole].Position = position;
-            m_RoleStates[iRole].IsArrival = INT_FALSE;
+            m_RoleStates[iRole].Position = RandPositionOnGround();
             m_RoleCommands[iRole].MoveTo = RandPositionOnGround();
             m_RoleCommands[iRole].Speed = MoveSpeed;
-            m_Roles[iRole].SetMoveTo(m_RoleCommands[iRole].MoveTo);
+            m_RoleResults[iRole].IsArrival = INT_FALSE;
         }
 
-        m_CB_IO_RoleState = new ComputeBuffer(RoleCount, Marshal.SizeOf(typeof(CB_IO_RoleState)));
-        m_CB_IO_RoleState.SetData(m_RoleStates);
+        m_CB_RoleState = new ComputeBuffer(RoleCount, Marshal.SizeOf(typeof(RoleState)));
+        m_CB_RoleState.SetData(m_RoleStates);
 
-        m_CB_I_RoleCommand = new ComputeBuffer(RoleCount, Marshal.SizeOf(typeof(CB_I_RoleCommand)));
-        m_CB_I_RoleCommand.SetData(m_RoleCommands);
+        m_CB_RoleCommand = new ComputeBuffer(RoleCount, Marshal.SizeOf(typeof(RoleCommand)));
+        m_CB_RoleCommand.SetData(m_RoleCommands);
 
-        m_AsyncGPUReadbackRequest = UnityEngine.Rendering.AsyncGPUReadback.Request(m_CB_IO_RoleState);
+        m_CB_RoleResult = new ComputeBuffer(RoleCount, Marshal.SizeOf(typeof(RoleResult)));
+        m_CB_RoleResult.SetData(m_RoleResults);
 
-        m_CS_UpdateKernel = MovementComputeShader.FindKernel("Update");
-        MovementComputeShader.SetBuffer(m_CS_UpdateKernel, "RoleStates", m_CB_IO_RoleState);
-        MovementComputeShader.SetBuffer(m_CS_UpdateKernel, "RoleCommands", m_CB_I_RoleCommand);
+        m_RoleArgs = new uint[5] { RoleMesh.GetIndexCount(0), (uint)RoleCount, 0, 0, 0 };
+        m_CB_RoleArgs = new ComputeBuffer(1
+            , (m_RoleArgs.Length * Marshal.SizeOf(typeof(uint)))
+            , ComputeBufferType.IndirectArguments);
+        m_CB_RoleArgs.SetData(m_RoleArgs);
+
+        MovementComputeShader.SetBuffer(m_CS_UpdateKernel, "RoleStates", m_CB_RoleState);
+        MovementComputeShader.SetBuffer(m_CS_UpdateKernel, "RoleCommands", m_CB_RoleCommand);
+        MovementComputeShader.SetBuffer(m_CS_UpdateKernel, "RoleResults", m_CB_RoleResult);
+        RoleMaterial.SetBuffer("RoleStates", m_CB_RoleState);
+
+        UnityEditor.SceneView.onSceneGUIDelegate += OnSceneGUI;
     }
 
-    protected void Update()
+    protected void OnDestroy()
     {
+        UnityEditor.SceneView.onSceneGUIDelegate -= OnSceneGUI;
+
+        m_CB_RoleCommand.Release();
+        m_CB_RoleState.Release();
+        m_CB_RoleResult.Release();
+        m_CB_RoleArgs.Release();
+    }
+
+    protected void LateUpdate()
+    {
+        bool needSendCommand = false;
         if (m_AsyncGPUReadbackRequest.done)
         {
             if (!m_AsyncGPUReadbackRequest.hasError)
             {
-                Unity.Collections.NativeArray<CB_IO_RoleState> resultBuffers = m_AsyncGPUReadbackRequest.GetData<CB_IO_RoleState>();
-                for (int iResult = 0; iResult < m_RoleStates.Length; ++iResult)
+                Unity.Collections.NativeArray<RoleResult> resultBuffers = m_AsyncGPUReadbackRequest.GetData<RoleResult>();
+                resultBuffers.CopyTo(m_RoleResults);
+                for (int iResult = 0; iResult < m_RoleResults.Length; ++iResult)
                 {
-                    m_RoleStates[iResult] = resultBuffers[iResult];
-                    m_Roles[iResult].transform.position = m_RoleStates[iResult].Position;
-                    if (m_RoleStates[iResult].IsArrival == INT_TRUE)
+                    if (m_RoleResults[iResult].IsArrival == INT_TRUE)
                     {
                         m_RoleCommands[iResult].MoveTo = RandPositionOnGround();
-                        m_Roles[iResult].SetMoveTo(m_RoleCommands[iResult].MoveTo);
+                        needSendCommand = true;
                     }
                 }
             }
@@ -80,13 +105,23 @@ public class MovementTest : MonoBehaviour
             {
                 Debug.LogError("AsyncGPUReadback Error");
             }
-            m_AsyncGPUReadbackRequest = UnityEngine.Rendering.AsyncGPUReadback.Request(m_CB_IO_RoleState);
+            m_AsyncGPUReadbackRequest = UnityEngine.Rendering.AsyncGPUReadback.Request(m_CB_RoleResult);
         }
 
-        m_CB_I_RoleCommand.SetData(m_RoleCommands);
+        if (needSendCommand)
+        {
+            m_CB_RoleCommand.SetData(m_RoleCommands);
+        }
         MovementComputeShader.SetFloat(SHADER_PROPERTYID_DELTATIME, Time.deltaTime);
 
-        MovementComputeShader.Dispatch(m_CS_UpdateKernel, 32, 1, 1);
+        MovementComputeShader.Dispatch(m_CS_UpdateKernel, RoleCount, 1, 1);
+
+        Draw(MainCamera);
+    }
+
+    private void OnSceneGUI(UnityEditor.SceneView sceneView)
+    {
+        Draw(sceneView.camera);
     }
 
     private Vector3 RandPositionOnGround()
@@ -96,17 +131,37 @@ public class MovementTest : MonoBehaviour
             , Random.Range(-GroundHalfSize.y, GroundHalfSize.y));
     }
 
-    [StructLayout(LayoutKind.Sequential)]
-    private struct CB_IO_RoleState
+    private void Draw(Camera camera)
     {
-        public Vector3 Position;
-        public int IsArrival;
+        Graphics.DrawMeshInstancedIndirect(RoleMesh
+            , 0
+            , RoleMaterial
+            , UNLIMITED_BOUNDS
+            , m_CB_RoleArgs
+            , 0
+            , null
+            , UnityEngine.Rendering.ShadowCastingMode.Off
+            , false
+            , 0
+            , camera);
     }
 
     [StructLayout(LayoutKind.Sequential)]
-    private struct CB_I_RoleCommand
+    private struct RoleState
+    {
+        public Vector3 Position;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct RoleCommand
     {
         public Vector3 MoveTo;
         public float Speed;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct RoleResult
+    {
+        public int IsArrival;
     }
 }
---
+++ Assets/Movement/Role.shader
@@ -0,0 +1,62 @@
+﻿Shader "Custom/Role"
+{
+    Properties
+    {
+        _MainTex ("Albedo (RGB)", 2D) = "white" {}
+    }
+    SubShader
+    {
+        Pass
+        {
+            Tags {"LightMode"="ForwardBase"}
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma multi_compile_fwdbase nolightmap nodirlightmap nodynlightmap novertexlight
+            #pragma target 4.5
+
+            struct RoleState
+            {
+                float3 Position;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            StructuredBuffer<RoleState> RoleStates;
+
+            struct appdata_custom {
+                uint instanceID : SV_InstanceID;
+                float4 vertex : POSITION;
+                float4 texcoord : TEXCOORD0;
+            };
+
+            struct v2f {
+                float4 pos : SV_POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            v2f vert (appdata_custom v)
+            {
+                float3 p = RoleStates[v.instanceID].Position;
+                float4x4 mat = {
+                    1,0,0,p.x,
+                    0,1,0,p.y,
+                    0,0,1,p.z,
+                    0,0,0,1,
+                };
+                float4 wpos = mul(mat, v.vertex);
+
+                v2f o;
+                o.pos = UnityObjectToClipPos(float4(wpos.xyz, 1));
+                o.uv = v.texcoord.xy;
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                return tex2D(_MainTex, i.uv);
+            }
+            ENDCG
+        }
+    }           
+}
